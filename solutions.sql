48f2ff0239d366c9d6d5d1b82c332aa33305b2cb
ahmedhamidibr <129907102+ahmedhamidibr@users.noreply.github.com>
2023-04-27 08:31:18 +0200

    Update solutions.sql

--- solutions.sql
+++ solutions.sql
@@ -5,11 +5,3 @@
   group by department
   order by num_employees desc 
   
--- question ID 9919
-
-  select salary, count(*) 
-  from employee
-  group by salary
-  having count (*) = 1
-  order by salary desc
-  limit 1
